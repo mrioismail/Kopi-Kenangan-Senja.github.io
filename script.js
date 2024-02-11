9de65457242424a502e91fc72443df730c0c8e80
mrioismail <muhamadrioismail@gmail.com>
2024-02-11 15:57:18 +0700

    menambahkan produk dan search

--- script.js
+++ script.js
@@ -6,9 +6,60 @@ hamburger.addEventListener('click', function () {
    navbarNav.classList.toggle('active')
 })
 
-// klik di luar sidebar untuk menghilangkan nav
+// klik di luar elemen
 document.addEventListener('click', function (e) {
    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
       navbarNav.classList.remove('active')
    }
+
+   if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+      searchForm.classList.remove('active')
+   }
+
+   if (!shopping.contains(e.target) && !shoppingCart.contains(e.target)) {
+      shoppingCart.classList.remove('active')
+   }
 })
+
+// search form
+const search = document.querySelector('#search')
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+search.addEventListener('click', function (e) {
+   searchForm.classList.toggle('active')
+   searchBox.focus()
+   // agar aksi default tidak berfungsi
+   e.preventDefault()
+})
+
+// shooping cart
+const shopping = document.querySelector('#shopping-cart')
+const shoppingCart = document.querySelector('.shopping-cart')
+
+shopping.addEventListener('click', function () {
+   shoppingCart.classList.toggle('active')
+})
+
+// detail item produk
+const itemModal = document.querySelector('#item-detail-modal')
+const itemDetail = document.querySelector('.item-detail')
+
+itemDetail.onclick = (e) => {
+   itemModal.style.display = 'flex'
+   e.preventDefault()
+}
+
+// // tombol clos detail item
+// document.querySelector('.modal .close-icon').onclick = (e) => {
+//    detailItem.style.display = 'none'
+//    e.preventDefault()
+// }
+
+// // klik di luar modal
+// const modal = document.querySelector('#item-detail-modal');
+// window.onclick = (e){
+//    if (e.target === modal){
+//       modal.style.display = 'none'
+//    }
+// }
